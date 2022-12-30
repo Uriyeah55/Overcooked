e8ef41c8bd181e29090d1c86e8fd4d3be040bab8
oriol mascaro <uri55577@gmail.com>
2022-12-30 14:22:05 +0100

    item picking up
    -started raycast
    -detecting tagged objects
    -testing picking ingredient

--- Assets/Scripts_Project/ItemInteraction.cs
+++ Assets/Scripts_Project/ItemInteraction.cs
@@ -4,20 +4,51 @@ using UnityEngine;
 
 public class ItemInteraction : MonoBehaviour
 {
+    GameObject player;
+    public GameObject holdingObjectPosition;
+    GameObject currentObj;
+
+   public GameObject rayCastOrigin;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentObj=null;
+        player = GameObject.Find("Player");
+           
     }
 
     // Update is called once per frame
     void Update()
     {
-        Ray ray = new Ray (this.transform.position + Camera.main.transform.forward * 0.3f, this.transform.forward);
+        if(currentObj != null)
+        {
+        currentObj.transform.position=holdingObjectPosition.transform.position;
+
+        }
+        Ray ray = new Ray (rayCastOrigin.transform.position + Camera.main.transform.forward * 0.3f, rayCastOrigin.transform.forward);
         Debug.DrawRay (ray.origin, ray.direction * 10, Color.red);
+
+       
 
         RaycastHit hit;
         if (Physics.Raycast (ray, out hit, 20))  
-            Debug.Log (hit.collider.name);
+        {
+
+        Debug.Log (hit.collider.name);
+        Debug.Log (hit.collider.tag);
+
+            if (hit.collider != null)
+            {
+                if(hit.collider.tag=="Pickable")
+                {
+                    if (Input.GetKeyDown(KeyCode.E))
+                    {
+                         //Destroy(hit.transform.gameObject);
+                         currentObj=hit.transform.gameObject;
+                    }
+                }       
+            }
+        }    
     }
 }
